a60efcb229af733502804e0b97bce0b80329c855
BL19 <30739672+BL19@users.noreply.github.com>
2022-07-20 14:41:37 +0200

    [WIP] TCP Communication

---
+++ Electrum.Communication.Tcp/RPCPacket.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Electrum.Communication.Tcp
+{
+    public class RPCPacket
+    {
+        public Guid Id { get; set; }
+        public string ServiceName { get; set; }
+        public string MethodName { get; set; }
+        public string[] ParameterTypes { get; set; }
+        public object[] Parameters { get; set; }
+
+        public object[] GetCorrectParameters()
+        {
+            // Redo the parameters into their correct types
+            var newParams = new object[Parameters.Length];
+            for (int i = 0; i < Parameters.Length; i++)
+            {
+                // Reformat as json
+                var json = JsonConvert.SerializeObject(Parameters[i]);
+                var type = Type.GetType(ParameterTypes[i]);
+                var reformattedType = JsonConvert.DeserializeObject(json, type);
+                newParams[i] = reformattedType;
+            }
+            return newParams;
+        }
+
+    }
+}
---
+++ Electrum.Communication.Tcp/TcpConnection.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Sockets;
+
+namespace Electrum.Communication.Tcp
+{
+    public class TcpConnection
+    {
+
+        private TcpClient Client { get; }
+        private IServiceProvider ServiceProvider { get; }
+
+        public TcpConnection(TcpClient client, IServiceProvider serviceProvider)
+        {
+            Client = client;
+            ServiceProvider = serviceProvider;
+        }
+
+    }
+}
